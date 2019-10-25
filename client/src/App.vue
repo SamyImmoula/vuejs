97280a56eee94237f3259ebc80f685fdd0031371
SamyImmoula <immoulasamy@gmail.com>
2019-10-25 10:15:59 +0200

    drag and drop

---
+++ client/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Board />
+  </div>
+</template>
+
+<script>
+import Board from './components/Board.vue'
+export default {
+  name: 'app',
+  components: {
+    Board
+  },
+}
+</script>
+
+<style>
+#app {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  width: 100%;
+  min-width: 100%;
+  max-width: 100%;
+  min-width: 100%;
+  max-width: 100%;
+}
+</style>
---
+++ client/src/components/NewCardPlusPlus.vue
@@ -0,0 +1,66 @@
+<template>
+    <Modal :open="open" :onClose="onClose" title="New Card">
+        <template slot="default">
+            <div class="grid-row">
+                <label for="title">Title</label>
+                <input v-model="name" id="title">
+            </div>
+            <div class="grid-row">
+                <label for="description">Description</label>
+                <textarea v-model="description" id="description" />
+            </div>
+            <div class="grid-row">
+                <label for="velocity">Velocity</label>
+                <input v-model.number="velocity" id="velocity">
+            </div>
+            <div class="grid-row">
+                <label>Status</label>
+                <label>
+                    New
+                    <input v-model="status" type="radio" value="new" name="status">
+                </label>
+                <label>
+                    Done
+                    <input v-model="status" type="radio" value="done" name="status">
+                </label>
+            </div>
+        </template>
+        <template slot="footer">
+            <button @click="handleSubmit">Submit</button>
+        </template>
+    </Modal>
+</template>
+
+<script>
+import Modal from './lib/Modal';
+export default {
+    name: "NewCard",
+    components: {
+        Modal,
+    },
+    data: () => ({
+        name: "",
+        description: "",
+        velocity: 0,
+        status: "new"
+    }),
+    props: {
+        open: Boolean,
+        onClose: Function
+    },
+    methods: {
+        handleSubmit: function () {
+            this.$emit('new-card', {...this.$data});
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .grid-row {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-content: center;
+    }
+</style>
